6dee816baf17474d8ddf32d5786a9fa714be38ef
RajanKumar5 <rajan.kumar.rk.0502@gmail.com>
2021-10-07 16:17:07 +0000

    Added validation check on login & signup

--- reactapp/src/components/LoginPage.js
+++ reactapp/src/components/LoginPage.js
@@ -1,21 +1,82 @@
 import React, { Component } from "react";
-import {Card, Form , Button, Col, Row} from 'react-bootstrap';
+import {Card, Form , Button, Col, Row, Alert} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 
 class LoginPage extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            show: false,
+            message: null
+        };
+    }
+
+    showAlert = () => {
+        var username = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+        
+        // Null Check
+        if(username === ""){
+            this.setState({
+                show: true,
+                message: "Email filed cannot be empty"
+            });
+            document.loginForm.email.focus();
+            return;
+        }
+        else if(password === ""){
+            this.setState({
+                show: true,
+                message: "Password field cannot be empty"
+            });
+            document.loginForm.password.focus();
+            return;
+        }
+
+        // Admin Authentication
+        if(username === "admin" && password === "admin"){
+            this.setState({
+                show: true,
+                message: "Welcome Admin"
+            });
+            return;
+        }
+
+        // Email Validaition
+        var mailformat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+        if(!username.match(mailformat))
+        {
+            this.setState({
+                show: true,
+                message: "Enter valid email"
+            });
+            document.loginForm.email.focus();
+            return;
+        }
+    }
+
     render(){
         return(
             <div className="container mt-5 mb-5">
                 <Row>
                     <Col lg={3}></Col>
                     <Col lg={6}>
+                        
+                        {this.state.show?
+                        // <Alert variant="danger">{this.state.message}</Alert>
+                        <Alert variant="danger" onClose={() => this.setState({show: false})} dismissible>
+                                {this.state.message}
+                        </Alert>
+                        :null}
+
                         <Card id="loginBox">
                             <Card.Header><b>LOGIN</b></Card.Header>
                             <Card.Body>
-                                <Form>
+                                <Form name="loginForm">
                                     <Form.Group className="mb-3" controlId="email">
                                         <Form.Label>Email address</Form.Label>
-                                        <Form.Control type="email" placeholder="Enter email" required/>
+                                        <Form.Control type="text" placeholder="Enter email" required/>
                                     </Form.Group>
 
                                     <Form.Group className="mb-3" controlId="password">
@@ -23,7 +84,7 @@ class LoginPage extends Component{
                                         <Form.Control type="password" placeholder="Password" required/>
                                     </Form.Group>
 
-                                    <Button size="sm" id="submitButton" variant="primary" type="submit">
+                                    <Button size="sm" id="submitButton" variant="primary" onClick={this.showAlert}>
                                         LOGIN
                                     </Button>
                                 </Form>
--- reactapp/src/components/SignupPage.js
+++ reactapp/src/components/SignupPage.js
@@ -1,18 +1,133 @@
 import React, { Component } from "react";
-import {Card, Form , Button, Col, Row} from 'react-bootstrap';
+import {Card, Form , Button, Col, Row, Alert} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 
 class SignupPage extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            show: false,
+            message: null
+        };
+    }
+
+    showAlert = () => {
+        var emailID = document.getElementById("email").value;
+        var username = document.getElementById("username").value;
+        var mobileNumber = document.getElementById("mobilenumber").value;
+        var password = document.getElementById("password").value;
+        var confirmPassword = document.getElementById("confirmpassword").value;
+
+        // Null Check
+        if(emailID === ""){
+            this.setState({
+                show: true,
+                message: "Email cannot be empty"
+            });
+            document.signupForm.email.focus();
+            return;
+        }
+        else if(username === ""){
+            this.setState({
+                show: true,
+                message: "Username cannot be empty"
+            });
+            document.signupForm.username.focus();
+            return;
+        }
+        else if(mobileNumber === ""){
+            this.setState({
+                show: true,
+                message: "Mobile number cannot be empty"
+            });
+            document.signupForm.mobilenumber.focus();
+            return;
+        }
+        else if(password === ""){
+            this.setState({
+                show: true,
+                message: "Password cannot be empty"
+            });
+            document.signupForm.password.focus();
+            return;
+        }
+        else if(confirmPassword === ""){
+            this.setState({
+                show: true,
+                message: "Confirm Password cannot be empty"
+            });
+            document.signupForm.confirmpassword.focus();
+            return;
+        }
+
+        // Email Validaition
+        var mailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+        if(!emailID.match(mailFormat))
+        {
+            this.setState({
+                show: true,
+                message: "Enter valid email"
+            });
+            document.signupForm.email.focus();
+            return;
+        }
+
+        // Username Validation
+        var usernameFormat = /^[a-zA-Z0-9]+$/;
+        if(!username.match(usernameFormat))
+        {
+            this.setState({
+                show: true,
+                message: "Enter valid username"
+            });
+            document.signupForm.username.focus();
+            return;
+        }
+
+        // Mobile Number Validation
+        var mobileFormat = /^[1-9]{1}[0-9]{9}$/;
+        if(!mobileNumber.match(mobileFormat))
+        {
+            this.setState({
+                show: true,
+                message: "Enter valid mobile number"
+            });
+            document.signupForm.mobilenumber.focus();
+            return;
+        }
+
+        // Password Validation
+        if(password !== confirmPassword){
+            this.setState({
+                show: true,
+                message: "Enter same password"
+            });
+            document.signupForm.password.focus();
+            document.signupForm.confirmpassword.focus();
+            return;
+        }
+
+    }
+
     render(){
         return(
             <div className="container mt-4 mb-5">
                 <Row>
                     <Col lg={3}></Col>
                     <Col lg={6}>
+
+                        {this.state.show?
+                            // <Alert variant="danger">{this.state.message}</Alert>
+                            <Alert variant="danger" onClose={() => this.setState({show: false})} dismissible>
+                                    {this.state.message}
+                            </Alert>
+                        :null}
+
                         <Card id="signupBox">
                             <Card.Header><b>SIGN UP</b></Card.Header>
                             <Card.Body>
-                                <Form>
+                                <Form name="signupForm">
                                     <Form.Group className="mb-3" controlId="email">
                                         <Form.Label>Email address</Form.Label>
                                         <Form.Control type="email" placeholder="Enter email" required/>
@@ -38,7 +153,7 @@ class SignupPage extends Component{
                                         <Form.Control type="password" placeholder="Confirm Password" required/>
                                     </Form.Group>
 
-                                    <Button size="sm" id="submitButton" variant="primary" type="submit">
+                                    <Button size="sm" id="submitButton" variant="primary" onClick={this.showAlert}>
                                         SIGN UP
                                     </Button>
                                 </Form>
